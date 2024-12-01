1ed1e26987d3c615aebd259a7fb8e49bfa0913f9
Patrick Ngabo <pa.ngabonziza@gmail.com>
2024-12-01 20:06:49 +0200

    fix dependancies

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -4,8 +4,9 @@ local null_ls = require "null-ls"
 local opts = {
   sources = {
     null_ls.builtins.formatting.prettier,
-    -- null_ls.builtins.diagnostics.eslint_d,
+    -- null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
+    require "none-ls.diagnostics.eslint", -- requires none-ls-extras.nvim
   },
   on_attach = function(client, bufnr)
     if client.supports_method "textDocument/formatting" then
